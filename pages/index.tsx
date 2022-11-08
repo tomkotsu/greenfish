ba81ea48d274ca9b5912e89f967543bc3464f693
Tom <tombinnie@protonmail.com>
2022-11-08 18:58:50 +0000

    begin styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Navbar from './navbar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
+    <div className="">
+      <Navbar />
     </div>
   )
 }
--- pages/navbar/index.tsx
+++ pages/navbar/index.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <section>
-      <div></div>
+    <section className="bg-green-700 p-12 absolute w-full border-b-orange-300 border-b-4">
+      <div className="flex justify-evenly items-center text-offwhite text-center ">
+        <h1 className="">LOGO</h1>
+        <h1 className="">GREENFISH</h1>
+        <ul className="flex gap-4 ">
+          <li>All Fish</li>
+          <li>Fish by Region</li>
+          <li>About</li>
+        </ul>
+      </div>
     </section>
   );
 };
